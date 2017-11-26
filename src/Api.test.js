6aff96371f818a627f5d95ea3ee243d507e01b41
Gabriel Ulloa <gsulloa@uc.cl>
2017-11-26 11:41:24 -0300

    API change (#2)
    * Using axios instead pospsicle
    
    * Added patch and delete. Updated tests

--- src/Api.test.js
+++ src/Api.test.js
@@ -1,30 +1,45 @@
 import Api from "./Api"
 
-const url = "http://ip.jsontest.com"
-const api = new Api(url)
+const url = "https://jsonplaceholder.typicode.com/"
 
-it("sets urls", () => {
-  expect(api.baseUrl).toBe(url)
-  expect(api.url("/users")).toBe(`${url}/users`)
-})
-
-it("gets", async () => {
-  const ip = await api.GET("/")
-  expect(ip).toHaveProperty("ip")
-})
-
-it("posts", async () => {
-  const ip = await api.POST("/", {})
-  expect(ip).toHaveProperty("ip")
-})
-
-it("fails parsing json", async () => {
-  const err = await api.request(new Promise(res => res("asd/adw")))
-  expect(err).toBeInstanceOf(Error)
-})
-
-it("fails on bad url", async () => {
-  const api = new Api("")
-  const err = await api.GET("/")
-  expect(err).toBeInstanceOf(Error)
+describe("API", () => {
+  let api
+  beforeEach(() => {
+    api = new Api(url)
+  })
+  it("sets urls", () => {
+    expect(api.baseUrl).toBe(url)
+    expect(api.url("/posts/1")).toBe(`${url}/posts/1`)
+  })
+  it("gets", async () => {
+    const post = await api.get("/posts/1")
+    expect(post).toHaveProperty("id")
+  })
+  it("posts", async () => {
+    const data = {
+      userId: 1,
+      title: "foo",
+      content: "bar",
+    }
+    const post = await api.post("/posts", data)
+    expect(post).toMatchObject(data)
+  })
+  it("patch", async () => {
+    const data = {
+      userId: 1,
+      title: "foo",
+      content: "bar",
+    }
+    const post = await api.patch("/posts/1", data)
+    expect(post).toMatchObject(data)
+  })
+  it("delete", async () => {
+    const post = await api.del("/posts/1")
+    expect(post).not.toBeInstanceOf(Error)
+  })
+  /*it("fails on bad url", async () => {
+    const api = new Api("")
+    const err = await api.get("/")
+    expect(err).toBeInstanceOf(Error)
+  })*/
 })
